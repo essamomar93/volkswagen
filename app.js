fef71c3ac0f7ec217c1ae88f0fb634922f847317
essamomar93 <>
2021-06-16 15:10:25 +0000

    add js file

---
+++ app.js
@@ -0,0 +1,32 @@
+/*
+lets start with V.W webbage
+
+ */
+var name = "essam";
+console . log (name)
+console . log ('name')
+
+var x=5
+var y=12
+var sum = x+y
+
+console .log(sum)
+
+
+alert("Welcome To A V.W Web")
+ 
+ var car = prompt("Do you have any type of V.W ?(yes or no) ")
+
+ if( car == "yes"){
+   document . write("<p>"+"You are with our team"+"</p>")
+ }
+
+else{
+  document . write ("<p>"+"We Hope to be one of V.W Owner"+"</p>")
+}
+
+if (confirm("Do you love v.w ?")) {
+  document . write("<p>"+"And we love you "+"</p>")
+} else {
+  document . write("<p>"+"after you visit our web you will"+"</p>")
+}
